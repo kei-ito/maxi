d0dd60c47a3a448789646decc90154d4e132455e
Kei Ito <kei.itof@gmail.com>
2019-04-23 17:26:38 +0900

    fix: fix endpoint

--- client/script/util/constants.ts
+++ client/script/util/constants.ts
@@ -1,7 +1,7 @@
 import {IFont} from '../types';
 
 export const developMode = 80 < Number(location.port);
-export const APIBaseURL = new URL(developMode ? `http://${location.hostname}:3000` : 'https://elgijkhadd.execute-api.ap-northeast-1.amazonaws.com/Prod');
+export const APIBaseURL = new URL(developMode ? `http://${location.hostname}:3000` : 'https://elgijkhadd.execute-api.ap-northeast-1.amazonaws.com/Prod/');
 export const AvailableFonts: Array<IFont> = ['Serif', 'Sans', 'Monospace'];
 export enum URLParameterKey {
     font = 'font',
