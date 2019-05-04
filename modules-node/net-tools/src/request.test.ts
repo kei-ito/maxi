1c3360627c3525f58ced550c45faa19626184999
Kei Ito <kei.itof@gmail.com>
2019-05-04 12:22:19 +0900

    chore(net-tools): use os.hostname

--- modules-node/net-tools/src/request.test.ts
+++ modules-node/net-tools/src/request.test.ts
@@ -1,19 +1,25 @@
 import test from 'ava';
 import * as http from 'http';
+import * as os from 'os';
 import {request} from './request';
 import {asString} from '@maxi-js/stream-tools';
 import {listen} from './listen';
-import {getHostFromAddressInfo} from './getHostFromAddressInfo';
 
 test('request as a string', async (t) => {
-    const server = await listen(http.createServer((req, res) => {
-        res.writeHead(200);
-        res.write(`${req.url}\n`);
-        req.pipe(res);
-    }));
-    const host = getHostFromAddressInfo(server.address());
-    const actual = await request(`http://${host}/foo`, asString);
-    server.close();
-    t.is(actual.statusCode, 200);
-    t.is(actual.body, '/foo\n');
+    const server = await listen(
+        http.createServer((req, res) => {
+            res.writeHead(200);
+            res.write(`${req.url}\n`);
+            req.pipe(res);
+        }),
+    );
+    const address = server.address();
+    if (address && typeof address === 'object') {
+        const actual = await request(`http://${os.hostname()}:${address.port}/foo`, asString);
+        server.close();
+        t.is(actual.statusCode, 200);
+        t.is(actual.body, '/foo\n');
+    } else {
+        t.is(address && typeof address, 'object');
+    }
 });
