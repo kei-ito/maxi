d6b749201bfd16494089e74880555a36ae9872cd
Kei Ito <kei.itof@gmail.com>
2019-05-05 23:38:08 +0900

    perf(data-viewer): load stylesheet asynchronously

--- modules-dom/data-viewer/src/script/app.ts
+++ modules-dom/data-viewer/src/script/app.ts
@@ -5,3 +5,14 @@ import {App} from './components/App';
 const appContainerElement = document.createElement('div');
 document.body.appendChild(appContainerElement);
 ReactDOM.render(createElement(App), appContainerElement);
+
+[
+    'https://cdn.maxi.wemo.me/fonts/Serif/cmun-serif.css',
+    'https://cdn.maxi.wemo.me/fonts/Sans/cmun-sans.css',
+    'https://cdn.maxi.wemo.me/fonts/Typewriter/cmun-typewriter-light.css',
+].forEach((cssURL) => {
+    const linkElement = document.createElement('link');
+    linkElement.rel = 'stylesheet';
+    linkElement.href = cssURL;
+    document.head.appendChild(linkElement);
+});
