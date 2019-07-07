40d8f3a5fb54aed9278d5f55c6cc71829298cc5d
Kei Ito <kei.itof@gmail.com>
2019-07-07 21:53:32 +0900

    fix(config-validator): normalize glob pattern

--- modules-node/config-validator/src/getPackageDirectoryList.ts
+++ modules-node/config-validator/src/getPackageDirectoryList.ts
@@ -5,7 +5,7 @@ export const getPackageDirectoryList = async (
     patterns: Array<string>,
     directory = process.cwd(),
 ): Promise<Array<string>> => (await globby(
-    patterns.map((pattern) => path.join(directory, pattern)),
+    patterns.map((pattern) => path.join(directory, pattern).split(path.sep).join('/')),
     {onlyDirectories: true},
 ))
 .sort();
